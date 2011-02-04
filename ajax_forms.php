b2cdc3ba0b793a1accb3a6d837976476bd5134fc
Steven Anderson <steve@whilefalse.net>
2011-02-04 14:21:52 +0000

    Loads of mods - need to clean this up at some point

--- ajax_forms.php
+++ ajax_forms.php
@@ -1,21 +1,35 @@
 <?php
 class AjaxFormsComponent extends Object{
       public $components = array('RequestHandler');
+	  public $doBeforeRender = true;
+	  public $viewToRender = false;
 
       public function startup($controller){
              $this->controller = $controller;
       }
 
       public function beforeRender(){
-             if ($this->controller->data && $this->RequestHandler->isAjax()){
+             if ($this->controller->data && $this->RequestHandler->isAjax() && $this->doBeforeRender){
                 $invalid = array();
                 $ok = true;
+				$rendered = null;
+
                 foreach($this->controller->data as $model=>$vals){
                     $this->controller->loadModel($model);
                     $invalid[$model] = $this->controller->{$model}->validationErrors;
                     if ($invalid[$model]) $ok = false;
                 }
-                $object = compact('ok', 'invalid');
+
+				//Render the view and save it if needed
+				if ($this->viewToRender){
+					$this->doBeforeRender = false;
+					$this->controller->autoLayout = false;
+					$rendered = $this->controller->render($this->viewToRender);
+				}
+
+
+				$data = $this->controller->data;
+                $object = compact('ok', 'invalid', 'data', 'rendered');
                 App::import('Helper', 'Javascript');
                 $js = new JavascriptHelper();
                 echo $js->object($object);
--- jquery.cake.form.js
+++ jquery.cake.form.js
@@ -1,28 +1,90 @@
 (function($){
     $.fn.cakeAjaxForm = function(options){
-        settings = {
+        var settings = {
             success: function(){},
-            failure: function(){}
+            failure: function(){},
+            beforeSubmit: function(){},
+            loadingSuccessSelector: '',
+            loadingSelector: 'div.submit',
+            loadingSuccessTimeout: 1000,
+            loadingFadeSpeed: 1000,
+            rowForm: false,
+            successElem: '<div class="content-box changes-saved">'+
+                           '<h4>Changes Saved</h4>'+
+                           '<p>Thank you, your changes have been saved.</p>'+
+                           '</div>'
         };
         $.extend(settings, options);
 
-        this.ajaxForm({
+        function relativeTo(form){
+            return (settings.rowForm ? form.parent() : form);
+        }
+        function loadingElem(form){
+            return (settings.loadingSelector ? $(settings.loadingSelector, relativeTo(form)) : form);
+        }
+        function loadingSuccessElem(form){
+            return (settings.loadingSuccessSelector ? $(settings.loadingSuccessSelector, relativeTo(form)) : form);
+        }
+
+        function traverseErrors(arr, name, callback){
+            if (typeof(arr) != 'string'){
+                for (key in arr){
+                    traverseErrors(arr[key], name+'['+key+']', callback);
+                }
+            }
+            else{
+                callback(name, arr);
+            }
+        }
+
+        this.live('submit', function(){
+            $(this).ajaxSubmit({
+                beforeSubmit: function(formData, jqForm, options){
+                    var cont = settings.beforeSubmit(formData, jqForm, options);
+                    if (cont){
+                        loadingElem(jqForm).addClass('loading');
+                    }
+                    return cont;
+                },
                 success: function(responseText, statusText, xhr, form){
                     eval('var json = '+responseText);
+
+                    $('div.error-message', relativeTo(form)).remove();
+                    loadingElem(form).removeClass('loading');
+                    setToSuccess(loadingSuccessElem(form), json.ok);
+
                     if (json.ok){
-                        settings.success(responseText, statusText, xhr, form);
+                        var success = $(settings.successElem).clone();
+                        $(form).find('input[type="submit"]').after(success);
+
+                        setTimeout(function(){
+                            success.fadeOut(500, function(){success.remove();});
+                        }, 3000);
+
+                        settings.success(responseText, statusText, xhr, form, json);
                     }
                     else{
-                        $('div.error-message', form).remove();
-                        for (var model in json.invalid){
-                            for (var field in json.invalid[model]){
-                                var name = 'data['+model+']['+field+']';
-                                $('*[name="'+name+'"]', form).after('<div class="error-message">'+json.invalid[model][field]+'</div>');
-                            }
-                        }
-                        settings.failure(responseText, statusText, xhr, form);
+                        traverseErrors(json.invalid, 'data', function(name, msg){
+                            $('*[name="'+name+'"]:last', relativeTo(form)).after('<div class="error-message">'+msg+'</div>');
+                        });
+
+                        settings.failure(responseText, statusText, xhr, form, json);
                     }
                 }
             });
+            return false;
+        });
+
+        //Generic loading gif stuff
+        function setToSuccess(elem, success){
+            elem.removeClass('loading');
+            elem.addClass('loading-'+(success ? 'success' : 'failure'));
+            setTimeout(
+                function(){
+                    elem.removeClass('loading-'+(success ? 'success' : 'failure'), settings.loadingFadeSpeed);
+                },
+                settings.loadingSuccessTimeout);
+        }
+
     }
 })(jQuery);
